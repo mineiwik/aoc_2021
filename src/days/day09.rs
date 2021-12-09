0d93de92f21b1aa7ae341bf1abacf6ffe698680e
mineiwik <29404520+mineiwik@users.noreply.github.com>
2021-12-09 07:59:03 +0100

    Reference madness (day 09)

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 pub fn solve() -> (String, String) {
@@ -25,11 +26,22 @@ fn part1(heightmap: &Vec<Vec<u32>>) -> String {
 fn part2(heightmap: &Vec<Vec<u32>>) -> String {
     let low_points: Vec<(usize, usize)> = find_low_points(heightmap).1;
 
-    let mut res: (u32, Vec<(usize, usize)>);
     let mut prod = Vec::new();
+
+    let mut visited: HashMap<(usize, usize), bool> = HashMap::new();
+
+    for y in 0..heightmap.len() {
+        for x in 0..heightmap[y].len() {
+            visited.insert((y, x), false);
+        }
+    }
+
     for low_point in low_points {
-        res = find_basin_size(heightmap, (low_point.0, low_point.1), Vec::new());
-        prod.push(res.0);
+        prod.push(find_basin_size(
+            heightmap,
+            (low_point.0, low_point.1),
+            &mut visited,
+        ));
     }
 
     prod.sort_by(|a, b| b.cmp(a));
@@ -40,38 +52,29 @@ fn part2(heightmap: &Vec<Vec<u32>>) -> String {
 fn find_basin_size(
     heightmap: &Vec<Vec<u32>>,
     coord: (usize, usize),
-    mut visited: Vec<(usize, usize)>,
-) -> (u32, Vec<(usize, usize)>) {
-    if heightmap[coord.0][coord.1] == 9 || visited.contains(&coord) {
-        return (0, visited);
+    visited: &mut HashMap<(usize, usize), bool>,
+) -> u32 {
+    if heightmap[coord.0][coord.1] == 9 || visited[&coord] {
+        return 0;
     }
 
     let mut sum = 1;
-    let mut res: (u32, Vec<(usize, usize)>);
-    visited.push(coord);
+    *visited.get_mut(&coord).unwrap() = true;
 
     if coord.0 < heightmap.len() - 1 {
-        res = find_basin_size(heightmap, (coord.0 + 1, coord.1), visited);
-        sum += res.0;
-        visited = res.1;
+        sum += find_basin_size(heightmap, (coord.0 + 1, coord.1), visited);
     }
     if coord.0 > 0 {
-        res = find_basin_size(heightmap, (coord.0 - 1, coord.1), visited);
-        sum += res.0;
-        visited = res.1;
+        sum += find_basin_size(heightmap, (coord.0 - 1, coord.1), visited);
     }
     if coord.1 < heightmap[0].len() - 1 {
-        res = find_basin_size(heightmap, (coord.0, coord.1 + 1), visited);
-        sum += res.0;
-        visited = res.1;
+        sum += find_basin_size(heightmap, (coord.0, coord.1 + 1), visited);
     }
     if coord.1 > 0 {
-        res = find_basin_size(heightmap, (coord.0, coord.1 - 1), visited);
-        sum += res.0;
-        visited = res.1;
+        sum += find_basin_size(heightmap, (coord.0, coord.1 - 1), visited);
     }
 
-    (sum, visited)
+    sum
 }
 
 fn find_low_points(heightmap: &Vec<Vec<u32>>) -> (u32, Vec<(usize, usize)>) {
