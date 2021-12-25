4040abfab8b104c4b8d6491b887a9b02eb8dd058
mineiwik <29404520+mineiwik@users.noreply.github.com>
2021-12-25 11:15:18 +0100

    Day 25

--- src/days/day25.rs
+++ src/days/day25.rs
@@ -2,16 +2,60 @@ use std::fs;
 
 pub fn solve() -> (String, String) {
     let stream = fs::read_to_string("inputs/day25.txt").unwrap();
-    let part1_sol = part1();
-    let part2_sol = part2();
+    let mut map: Vec<Vec<char>> = Vec::new();
 
-    (part1_sol, part2_sol)
+    for line in stream.lines() {
+        let mut map_line: Vec<char> = Vec::new();
+        for pos in line.chars() {
+            map_line.push(pos);
+        }
+        map.push(map_line);
+    }
+
+    let mut count = 1;
+
+    while step(&mut map) != 0 {
+        count += 1;
+    }
+
+    (count.to_string(), "Sleigh remotely started!".to_string())
 }
 
-fn part1() -> String {
-    "part1".to_string()
-}
-
-fn part2() -> String {
-    "part2".to_string()
+fn step(map: &mut Vec<Vec<char>>) -> usize {
+    let mut changes = 0;
+    let mut new_map = map.clone();
+    for i in 0..map.len() {
+        for j in 0..map[i].len() {
+            if map[i][j] == '>' {
+                let mut next_pos = j + 1;
+                if j == map[i].len() - 1 {
+                    next_pos = 0;
+                }
+                if map[i][next_pos] == '.' {
+                    changes += 1;
+                    new_map[i][j] = '.';
+                    new_map[i][next_pos] = '>';
+                }
+            }
+        }
+    }
+    *map = new_map;
+    let mut new_map = map.clone();
+    for i in 0..map.len() {
+        for j in 0..map[i].len() {
+            if map[i][j] == 'v' {
+                let mut next_pos = i + 1;
+                if i == map.len() - 1 {
+                    next_pos = 0;
+                }
+                if map[next_pos][j] == '.' {
+                    changes += 1;
+                    new_map[i][j] = '.';
+                    new_map[next_pos][j] = 'v';
+                }
+            }
+        }
+    }
+    *map = new_map;
+    changes
 }
